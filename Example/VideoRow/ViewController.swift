ea9c2852c918b1fbf8eda849473a02749d2e9584
Smiller193 <shawn.miller@temple.edu>
2018-11-09 13:33:54 -0500

    Finished example code and updated repo to contain all files needed to function properly

--- Example/VideoRow/ViewController.swift
+++ Example/VideoRow/ViewController.swift
@@ -29,10 +29,29 @@ class ViewController: FormViewController {
                     cell.accessoryView?.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
                     print(row.value as Any)
                 })
-        
+    }
+    
+    @objc func grabValues(){
+        //the dictionary of values that are returned from a Eureka row
+        let valuesDictionary = form.values()
+       //guard statement to assure proper handling of any potential nil TLPHAsset
+        guard let promoVideo = valuesDictionary["eventPromoVideoTag"] as? TLPHAsset else {
+            return
+        }
+        //must convert TLPHAsset to a regular phasset to make use of function
+        //TLPHAsset already includes the ability to do this so all you have to do as access the property on whatever TLPHAsset you have
+        guard let asset = promoVideo.phAsset else {
+            return
+        }
+        //once you have the asset use the function below to convert it into a url to use later. In the event that you want to upload it to firebase or aws or whatever your backend is
+        self.getUrlFromPHAsset(asset: asset) { (url) in
+            //do some action with url here
+        }
     }
     
     
+    
+    //use this to get url to submit to some backend service
     func getUrlFromPHAsset(asset: PHAsset, callBack: @escaping (_ url: URL?) -> Void)
     {
         asset.requestContentEditingInput(with: PHContentEditingInputRequestOptions(), completionHandler: { (contentEditingInput, dictInfo) in
